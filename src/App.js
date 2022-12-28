0a196c39619d30da002587c57437b7a33ddbd660
jhwmac <jkr2505@naver.com>
2022-12-28 15:08:38 +0900

    FEAT: API 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
+  const [ data, setData ] = useState('');
+  const onClick = () => {
+    axios(
+      "https://public.api.nexon.com/openapi/maplestory/v1/cube-use-results?count=10&date=2022-12-27&cursor="
+    ).then((res) => {
+      setData(res);
+    });
+  };
   return (
     <div className="App">
+      <button onClick={onClick}></button>
+      <div>
+      {data}
+      </div>
     </div>
   );
 }
